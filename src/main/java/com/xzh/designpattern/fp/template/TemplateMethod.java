7c013fa12db54f74fadd002946aeb06317aa10d6
xiaozh <xiaozh620>
2022-09-09 14:51:19 +0800

    feat: refactor template method to functional way

--- src/main/java/com/xzh/designpattern/fp/template/TemplateMethod.java
+++ src/main/java/com/xzh/designpattern/fp/template/TemplateMethod.java
@@ -1,21 +1,47 @@
 package com.xzh.designpattern.fp.template;
 
+
+
+
+import java.util.function.Function;
+
 public class TemplateMethod {
+
+    public static void main(String[] args) {
+        Vehicle blueBus = VehicleType.BUS.factory.apply(VehicleColor.BLUE);
+        blueBus.start(() -> System.out.println("bus preCheck"));
+    }
 
 
 }
 
 interface Vehicle{
 
-}
-
-abstract class AbstractVehicle implements Vehicle{
-
-    public void start() {
-        preStartCheck();
+    default void start(Runnable preStartChecks) {
+        preStartChecks.run();
         System.out.println(String.format("%s is starting...", this.getClass().getSimpleName()));
     }
 
-    protected abstract void preStartCheck();
-
 }
+
+enum VehicleType {
+    BUS(Bus::new);
+
+    public final Function<VehicleColor, Vehicle> factory;
+
+    VehicleType(Function<VehicleColor, Vehicle> factory) {
+        this.factory = factory;
+    }
+}
+
+enum VehicleColor {
+    RED, BLUE, GREEN
+}
+
+class Bus implements Vehicle {
+
+    public Bus(VehicleColor vehicleColor) {
+
+    }
+}
+
